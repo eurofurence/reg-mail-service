fcddbb96580f2f6de07b5add265ef6b0a7155ec5
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2023-01-30 22:23:31 +0100

    fix(#23): some cleanup and improved logging

--- internal/service/mailsrv/mailsrv.go
+++ internal/service/mailsrv/mailsrv.go
@@ -6,35 +6,31 @@ import (
 	"github.com/eurofurence/reg-mail-service/internal/api/v1/mail"
 	"github.com/eurofurence/reg-mail-service/internal/entity"
 	"github.com/eurofurence/reg-mail-service/internal/repository/config"
-	"github.com/eurofurence/reg-mail-service/internal/web/util/ctxvalues"
 	"gopkg.in/gomail.v2"
+	"strings"
 )
 
 type MailServiceImplData struct {
 }
 
-func (s *MailServiceImplData) SendMail(ctx context.Context, dto mail.MailSendDto, template entity.Template, body string, preview bool) error {
+func (s *MailServiceImplData) SendMail(ctx context.Context, dto mail.MailSendDto, template entity.Template, body string) error {
 	// Create a new message and set sender
 	m := gomail.NewMessage()
 	m.SetHeader("From", config.EmailFrom())
 
 	// Set recipients
-	if !preview {
-		if config.MailDevMode() {
-			m.SetHeader("To", config.MailDevMails()...)
-		} else {
-			m.SetHeader("To", dto.To...)
-
-			if len(dto.Cc) > 0 {
-				m.SetHeader("Cc", dto.Cc...)
-			}
-
-			if len(dto.Bcc) > 0 {
-				m.SetHeader("Bcc", dto.Bcc...)
-			}
-		}
+	if config.MailDevMode() {
+		m.SetHeader("To", config.MailDevMails()...)
 	} else {
-		m.SetHeader("To", ctxvalues.Email(ctx))
+		m.SetHeader("To", dto.To...)
+
+		if len(dto.Cc) > 0 {
+			m.SetHeader("Cc", dto.Cc...)
+		}
+
+		if len(dto.Bcc) > 0 {
+			m.SetHeader("Bcc", dto.Bcc...)
+		}
 	}
 
 	// Set subject & Body
@@ -53,12 +49,27 @@ func (s *MailServiceImplData) SendMail(ctx context.Context, dto mail.MailSendDto
 
 		err = d.DialAndSend(m)
 
-		aulogging.Logger.Ctx(ctx).Info().Printf("Mail with template (%s/%s) sent. TO: %s. CC: %s. BCC: %s",
-			dto.CommonID, dto.Lang, dto.To, dto.Cc, dto.Bcc)
+		aulogging.Logger.Ctx(ctx).Info().Printf("Mail with template (%s/%s) sent.", dto.CommonID, dto.Lang)
+		logTargets(ctx, dto)
+		aulogging.Logger.Ctx(ctx).Info().Printf("Subject: %s", template.Subject)
 	} else {
 		aulogging.Logger.Ctx(ctx).Info().Printf("Mail body with template (%s/%s) logged below (**not** sent).", dto.CommonID, dto.Lang)
+		logTargets(ctx, dto)
+		aulogging.Logger.Ctx(ctx).Info().Printf("Subject: %s", template.Subject)
 		aulogging.Logger.Ctx(ctx).Info().Printf(body)
 	}
 
 	return err
+}
+
+func logTargets(ctx context.Context, dto mail.MailSendDto) {
+	if len(dto.To) > 0 {
+		aulogging.Logger.Ctx(ctx).Info().Printf("To: %s", strings.Join(dto.To, ", "))
+	}
+	if len(dto.Cc) > 0 {
+		aulogging.Logger.Ctx(ctx).Info().Printf("Cc: %s", strings.Join(dto.Cc, ", "))
+	}
+	if len(dto.Bcc) > 0 {
+		aulogging.Logger.Ctx(ctx).Info().Printf("Bcc: %s", strings.Join(dto.Bcc, ", "))
+	}
 }
