8c12025bdb122667df913652a52df9d1dae35578
Jumpy Squirrel <jsquirrel_github_9a6d@packetloss.de>
2023-02-16 21:04:58 +0100

    fix(#38): relax email patterns

--- internal/repository/config/validation_test.go
+++ internal/repository/config/validation_test.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"github.com/stretchr/testify/require"
+	"regexp"
+	"testing"
+)
+
+func TestMailRegexp(t *testing.T) {
+	re := regexp.MustCompile(mailRegexp)
+
+	require.True(t, re.MatchString("a@b"))
+	require.True(t, re.MatchString("jsquirrel_github_9a6d@packetloss.de"))
+	require.True(t, re.MatchString("Super1+Super0-9Unusual324oi9e73289472347@Hihihi.there.com"))
+	require.False(t, re.MatchString(" space@start"))
+	require.False(t, re.MatchString("space@end "))
+	require.False(t, re.MatchString("space@in side"))
+	require.False(t, re.MatchString("more space@inside.de"))
+}
