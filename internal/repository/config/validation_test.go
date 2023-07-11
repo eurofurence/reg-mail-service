ce3eefcb6dcc318acb1a6d2c8fe9740bfc638b3d
Selis <selis@xynolabs.com>
2023-07-11 21:07:48 +0200

    Updated config tests

--- internal/repository/config/validation_test.go
+++ internal/repository/config/validation_test.go
@@ -9,11 +9,13 @@ import (
 func TestMailRegexp(t *testing.T) {
 	re := regexp.MustCompile(mailRegexp)
 
-	require.True(t, re.MatchString("a@b"))
-	require.True(t, re.MatchString("jsquirrel_github_9a6d@packetloss.de"))
-	require.True(t, re.MatchString("Super1+Super0-9Unusual324oi9e73289472347@Hihihi.there.com"))
-	require.False(t, re.MatchString(" space@start"))
-	require.False(t, re.MatchString("space@end "))
-	require.False(t, re.MatchString("space@in side"))
-	require.False(t, re.MatchString("more space@inside.de"))
+	require.True(t, re.MatchString("A <a@b>"))
+	require.True(t, re.MatchString("J Squirrel <jsquirrel_github_9a6d@packetloss.de>"))
+	require.True(t, re.MatchString("Super1+Super0-9Unusual324oi9e73289472347 <Super1+Super0-9Unusual324oi9e73289472347@Hihihi.there.com>"))
+	require.False(t, re.MatchString("Space < space@start>"))
+	require.False(t, re.MatchString("Space <space@end >"))
+	require.False(t, re.MatchString("Space <space@in side>"))
+	require.False(t, re.MatchString("Space <more space@inside.de>"))
+	require.False(t, re.MatchString("no_tags_for_name@defined.de"))
+	require.False(t, re.MatchString("Empty Tags <>"))
 }
